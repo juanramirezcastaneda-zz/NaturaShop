1b023f77be7419ffc14b383c72f0e531000d1d66
Juan Sebastian Ramirez Castañeda <juanramirezcastaneda@gmail.com>
2018-10-11 16:18:09 -0500

    Fix react warnings for keys.

--- Web/ClientApp/src/shared/CustomTable.js
+++ Web/ClientApp/src/shared/CustomTable.js
@@ -4,32 +4,31 @@ export class CustomTable extends Component {
   constructor(props) {
     super(props);
     const srcArray = props.src ? props.src : [];
-    const columns = srcArray.length ? Object.keys(srcArray[0]) : null;
-    this.state = { src: srcArray, columns: columns };
+    const columns = srcArray.length ? Object.keys(srcArray[0]) : [];
+    const prefix = props.prefix ? props.prefix : "ct";
+    this.state = { src: srcArray, columns: columns, prefix: prefix };
   }
   render() {
     return (
       <table>
         <thead key="thead">
           <tr>
-            {this.state.columns &&
-              this.state.columns.map(function(col) {
-                const camelCol = col.replace(/^\w/, c => c.toUpperCase());
-                return <td key={`h${col}`}>{camelCol}</td>;
-              }, this)}
+            {this.state.columns.map(function(col) {
+              const camelCol = col.replace(/^\w/, c => c.toUpperCase());
+              return <td key={`h${this.state.prefix}${col}`}>{camelCol}</td>;
+            }, this)}
           </tr>
         </thead>
         <tbody key="tbody">
-          {this.state.src &&
-            this.state.src.map(function(el) {
-              return (
-                <tr key={`r{$el.Id}`}>
-                  {this.state.columns.map(function(col) {
-                    return <td key={el[col]}>{el[col]}</td>;
-                  })}
-                </tr>
-              );
-            }, this)}
+          {this.state.src.map(function(el) {
+            return (
+              <tr key={`r${this.state.prefix + el.id}`}>
+                {this.state.columns.map(function(col) {
+                  return <td key={`${col}`}>{el[col]}</td>;
+                })}
+              </tr>
+            );
+          }, this)}
         </tbody>
       </table>
     );
